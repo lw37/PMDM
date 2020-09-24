270cadae2e1f016633fa80ddaf9367892320c4be
Wei Luo <luo.luo.ll14@gmail.com>
2020-09-24 18:14:36 +0200

    He terminado act1 , 2 y 3

---
+++ AE1/main.js
@@ -0,0 +1,20 @@
+//1.
+const ALERT_MESSAGE ="Soy el primer script";
+let a=ALERT_MESSAGE+"y estoy funcionando sobre chrom" 
+alert(a);
+//2.
+let meses = ['Enero', 'Febrero','Marzo','Abril','Mayo','Junio',
+            'Julio','Agosto','Septiembre','Octubre','Novienbre','Diciembre'];
+
+
+console.table(meses)
+//3.
+
+const values = [true, false, 5, "hola", [1,2,3], {age: 2, gender: 'male'}];
+for (let i = 0; i < values.length; i++) {
+    
+    console.log(typeof values[i]);
+}
+
+//4.
+
---
+++ main.js
@@ -0,0 +1,38 @@
+//1.
+const ALERT_MESSAGE ="Soy el primer script";
+let a=ALERT_MESSAGE+"y estoy funcionando sobre chrom" 
+alert(a);
+//2.
+let meses = ['Enero', 'Febrero','Marzo','Abril','Mayo','Junio',
+            'Julio','Agosto','Septiembre','Octubre','Novienbre','Diciembre'];
+
+
+console.table(meses)
+//3
+
+const values = [true, false, 5, "hola", [1,2,3], {age: 2, gender: 'male'}];
+for (let i = 0; i < values.length; i++) {
+    
+    console.log(typeof values[i]);
+}
+//4
+let n1=5;
+let n2=8;
+
+if(n1<n2){
+    console.log("numero1  no es mayor que numero2");
+}
+
+if(n2>0){
+    console.log("numero2 es positivo");
+}
+
+if(n1<0 || n1!=0){
+    console.log("n1 es negativo o distinto de 0. ");
+}
+if(n1++<=n2){
+    console.log("incrementar en 1 unidad de numero1 no lo hace mayor o igula que numero2 ");
+}
+for (let     = 0; index < array.length; index++)   nst element = array[index];
+    
+}

923e2cf05a779c81b27cb780d0c64b5b63aa371b
Wei Luo <luo.luo.ll14@gmail.com>
2020-10-08 22:17:35 +0200

    He terminado act2

--- ae2-1.js
+++ ae2-1.js
@@ -1,3 +1,4 @@
+//1A
 const array = ["Ana", 2, "Javi", 5, "Roberto", 7, "Vanessa", 10];
 
 const newarry1 = array.filter(i => {
@@ -5,7 +6,7 @@ const newarry1 = array.filter(i => {
         return i
 }
 )
-
+//1B
 console.log(newarry1);
 
 const newarry2 = array.filter(i => {

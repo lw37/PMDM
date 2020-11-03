373df17696eae34ea0cf15b965faf587740d124a
Wei Luo <luo.luo.ll14@gmail.com>
2020-11-04 00:09:40 +0100

    Creando la validacion de los datos

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const Continuar = () => {
     const rl = document.getElementById("CallU");
     const div1 = document.getElementById("d1");
     const div2 = document.getElementById("d2");
+    const name=document.getElementById("nombre");
+    const apel=document.getElementById("apellido");
+    const dateNaci=document.getElementById("fechaNacimiento");
 
     document.addEventListener('click', () => {
         if (rl.value !== "") {
@@ -19,7 +22,13 @@ const Continuar = () => {
         div1.style.display = "none";
         div2.style.display = "block";
         btc.addEventListener('click', () => {
-            
+            if(name.value!==""){
+                if(apel.value!==""){
+                    if(dateNaci!==""){
+                        
+                    }
+                }
+            }
 
         });
     });

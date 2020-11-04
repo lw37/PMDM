3c3f8c2cf90f88fb8a9c99d44f56381dbd6028c6
Wei Luo <luo.luo.ll14@gmail.com>
2020-11-04 23:19:43 +0100

    AE3:Acabado la funcion de imprimir AE3.

--- main.js
+++ main.js
@@ -1,37 +1,240 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     Continuar();
+
 });
 const Continuar = () => {
+    let pag = 1;
+    let val = 1;
+    const blk1 = document.getElementById("block1");
+    blk1.style.backgroundColor = "#4CAF50";
+    const blk2 = document.getElementById("block2");
+    const blk3 = document.getElementById("block3");
+    const blk4 = document.getElementById("block4");
+
     const btc = document.getElementById("continuar");
     const bta = document.getElementById("atras");
     const rl = document.getElementById("CallU");
+
     const div1 = document.getElementById("d1");
     const div2 = document.getElementById("d2");
-    const name=document.getElementById("nombre");
-    const apel=document.getElementById("apellido");
-    const dateNaci=document.getElementById("fechaNacimiento");
+    const div3 = document.getElementById("d3");
 
-    document.addEventListener('click', () => {
+    const div4 = document.getElementById("d4");
+
+    const nameER = document.getElementById("nombreER");
+    const apelER = document.getElementById("apellidoER");
+    const dateNaciER = document.getElementById("fechaER");
+    const codPosER = document.getElementById("codigoER");
+    const municER = document.getElementById("municipioER")
+
+
+
+    const infor = document.getElementById("informacion");
+    const name = document.getElementById("nombre");
+    const apel = document.getElementById("apellido");
+    const dateNaci = document.getElementById("fechaNacimiento");
+    const direc = document.getElementById("direccion");
+    const codPos = document.getElementById("codPostal");
+    const prov = document.getElementById("provincia");
+    const munic = document.getElementById("municipio");
+
+    apel.addEventListener("input", () => {
+        if (name.value === "") {
+            nameER.style.display = "block";
+        } else {
+            nameER.style.display = "none";
+        }
+    });
+    dateNaci.addEventListener("input", () => {
+        if (name.value === "") {
+            nameER.style.display = "block";
+        } else {
+            nameER.style.display = "none";
+        }
+        if (apel.value === "") {
+            apelER.style.display = "block";
+        } else {
+            apelER.style.display = "none";
+        }
+    });
+    codPos.addEventListener("input", () => {
+        if (name.value === "") {
+            nameER.style.display = "block";
+        } else {
+            nameER.style.display = "none";
+        }
+        if (apel.value === "") {
+            apelER.style.display = "block";
+        } else {
+            apelER.style.display = "none";
+        }
+        if (dateNaci.value === "") {
+            dateNaciER.style.display = "block";
+        } else {
+            dateNaciER.style.display = "none";
+        }
+    });
+    munic.addEventListener("input", () => {
+        munic.value = munic.value.toLocaleUpperCase();
+        if (name.value === "") {
+            nameER.style.display = "block";
+        } else {
+            nameER.style.display = "none";
+        }
+        if (apel.value === "") {
+            apelER.style.display = "block";
+        } else {
+            apelER.style.display = "none";
+        }
+        if (dateNaci.value === "") {
+            dateNaciER.style.display = "block";
+        } else {
+            dateNaciER.style.display = "none";
+        }
+        if (codPos.value > 99999 || codPos.value < 10000) {
+            codPosER.style.display = "block";
+        } else {
+            codPosER.style.display = "none";
+        }
+        if (munic.value == "") {
+            municER.style.display = "block";
+        } else {
+            municER.style.display = "none";
+        }
+        if (
+            (municER.style.display && codPosER.style.display && dateNaciER.style.display && apelER.style.display && nameER.style.display) == "none"
+        ){
+            btc.disabled = false;
+        }
+
+    });
+    rl.addEventListener('input', () => {
         if (rl.value !== "") {
             btc.disabled = false;
         }
     });
     btc.addEventListener('click', () => {
-        bta.style.display = "block";
-        div1.style.display = "none";
-        div2.style.display = "block";
-        btc.addEventListener('click', () => {
-            if(name.value!==""){
-                if(apel.value!==""){
-                    if(dateNaci!==""){
-                        
-                    }
-                }
-            }
+        pag++;
+        val++;
+        if (pag == 2) {
+            blk1.style.backgroundColor = "#333";
+            blk2.style.backgroundColor = "#4CAF50";
+            blk3.style.backgroundColor = "#333";
+            blk4.style.backgroundColor = "#333";
+            bta.style.display = "block";
+            btc.disabled = true;
+            div1.style.display = "none";
+            div2.style.display = "block";
+            div3.style.display = "none";
+            div4.style.display = "none";
+        }
+        /* if (val === 3) {
+             pag=2;
+             val=1;
+ 
+             if (name.value === "") {
+                 pag=2;
+                 val=1;        
+                 if (apel.value === "") {
+                     pag=2;
+                     val=1;            
+                     if (dateNaci.value === "") {
+                         pag=2;
+                         val=1;                   
+                         if (codPos.maxlength !== 5) {
+                             pag=2;
+                             val=1;                                                    
+                             if (prov.value === "") {                            
+                                 pag=2;
+                                 val=1;
+                                 if (munic.value === "") {
+                                     pag=2;
+                                     val=1;
+                                 }
+                             }
+                         }
+                     }
+                 }
+             }
+         }*/
+        if (pag == 3) {
+            const datos = {
+                web: rl.value, nombre: name.value, apellido: apel.value, FechaNacimiento: dateNaci.value, direccion: direc.value,
+                codigoPostal: codPos.value, provincia: prov.value, municipio: munic.value
+            };
+            infor.textContent="Web: "+datos.web+"\n Nombre: "+datos.nombre+"\n Apellido: "+datos.apellido+"\n Fecha Nacimiento: "+datos.FechaNacimiento+
+            "\n Direccion: "+datos.direccion+"\n Codigo Postal: "+datos.codigoPostal+"\n Provincia: "+datos.provincia+"\n Municipio: "+datos.municipio;
+            blk1.style.backgroundColor = "#333";
+            blk2.style.backgroundColor = "#333";
+            blk3.style.backgroundColor = "#4CAF50";
+            blk4.style.backgroundColor = "#333";
+            div1.style.display = "none";
+            div2.style.display = "none";
+            div3.style.display = "block";
+            div4.style.display = "none";
 
-        });
+        }
+        if (pag == 4) {
+            blk1.style.backgroundColor = "#333";
+            blk2.style.backgroundColor = "#333";
+            blk3.style.backgroundColor = "#333";
+            blk4.style.backgroundColor = "#4CAF50";
+            btc.style.display = "none";
+            div1.style.display = "none";
+            div2.style.display = "none";
+            div3.style.display = "none";
+            div4.style.display = "block";
+        }
     });
+
+
+
+
+
+
+
+
+    bta.addEventListener('click', () => {
+        pag--;
+        if (pag == 3) {
+            blk1.style.backgroundColor = "#333";
+            blk2.style.backgroundColor = "#333";
+            blk3.style.backgroundColor = "#4CAF50";
+            blk4.style.backgroundColor = "#333";
+            btc.style.display = "block";
+            bta.style.display = "block";
+            div1.style.display = "none";
+            div2.style.display = "none";
+            div3.style.display = "block";
+            div4.style.display = "none";
+        }
+        if (pag == 2) {
+            blk1.style.backgroundColor = "#333";
+            blk2.style.backgroundColor = "#4CAF50";
+            blk3.style.backgroundColor = "#333";
+            blk4.style.backgroundColor = "#333";
+            bta.style.display = "block";
+            btc.style.display = "block";
+            div1.style.display = "none";
+            div2.style.display = "block";
+            div3.style.display = "none";
+            div4.style.display = "none";
+        }
+        if (pag == 1) {
+            blk1.style.backgroundColor = "#4CAF50";
+            blk2.style.backgroundColor = "#333";
+            blk3.style.backgroundColor = "#333";
+            blk4.style.backgroundColor = "#333";
+            bta.style.display = "none";
+            btc.style.display = "block";
+            div1.style.display = "block";
+            div2.style.display = "none";
+            div3.style.display = "none";
+            div4.style.display = "none";
+        }
+    });
+
 
 }
 

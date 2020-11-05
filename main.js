844891609c5f001d8e18f5e590831f9ff80c81b8
Wei Luo <luo.luo.ll14@gmail.com>
2020-11-05 02:02:07 +0100

    AE3:Funciona el boton Aceptar del paso3

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-
     validaciones();
     botonContinuar();
     botonAtras();
+    
 });
 
 
 let pag = 1;
+
 const blk1 = document.getElementById("block1");
 blk1.style.backgroundColor = "#4CAF50";
 const blk2 = document.getElementById("block2");
 const blk3 = document.getElementById("block3");
 const blk4 = document.getElementById("block4");
 
-
+const btTer=document.getElementById("terminar");
+const btAcep=document.getElementById("aceptar");
 const btc = document.getElementById("continuar");
 const bta = document.getElementById("atras");
 const rl = document.getElementById("CallU");
@@ -22,7 +23,6 @@ const rl = document.getElementById("CallU");
 const div1 = document.getElementById("d1");
 const div2 = document.getElementById("d2");
 const div3 = document.getElementById("d3");
-
 const div4 = document.getElementById("d4");
 
 const nameER = document.getElementById("nombreER");
@@ -30,7 +30,6 @@ const apelER = document.getElementById("apellidoER");
 const dateNaciER = document.getElementById("fechaER");
 const codPosER = document.getElementById("codigoER");
 const municER = document.getElementById("municipioER")
-
 
 const infor1 = document.getElementById("informacion1");
 const infor = document.getElementById("informacion");
@@ -123,6 +122,70 @@ const validaciones = () => {
 }
 
 const botonContinuar = () => {
+    btAcep.addEventListener('click', () => {
+        pag++;
+
+        if (pag == 2) {
+
+            blk1.style.backgroundColor = "#333";
+            blk2.style.backgroundColor = "#4CAF50";
+            blk3.style.backgroundColor = "#333";
+            blk4.style.backgroundColor = "#333";
+            bta.style.display = "block";
+            btc.disabled = true;
+            div1.style.display = "none";
+            div2.style.display = "block";
+            div3.style.display = "none";
+            div4.style.display = "none";
+            if (
+                (municER.style.display && codPosER.style.display && dateNaciER.style.display && apelER.style.display && nameER.style.display) == "none"
+            ) {
+                btc.disabled = false;
+            }
+        }
+
+        if (pag == 3) {
+            const datos = {
+                web: rl.value, nombre: name.value, apellido: apel.value, FechaNacimiento: dateNaci.value, direccion: direc.value,
+                codigoPostal: codPos.value, provincia: prov.value, municipio: munic.value
+            };
+            infor.innerHTML = "";
+            infor.innerHTML = infor.innerHTML + "Web: " + datos.web + "<br> Nombre: " + datos.nombre + "<br> Apellido: " + datos.apellido + "<br> Fecha Nacimiento: " + datos.FechaNacimiento +
+                "<br> Direccion: " + datos.direccion + "<br> Codigo Postal: " + datos.codigoPostal + "<br> Provincia: " + datos.provincia + "<br> Municipio: " + datos.municipio;
+
+
+            /*            let datos1 = [rl.value, name.value, apel.value, dateNaci.value, direc.value, codPos.value, prov.value, munic.value];
+                        datos1.forEach(element =>{
+                            const fila =document.createElement("li");
+                            fila.appendChild(element);
+                            infor1.appendChild(fila);}
+                            );*/
+
+            blk1.style.backgroundColor = "#333";
+            blk2.style.backgroundColor = "#333";
+            blk3.style.backgroundColor = "#4CAF50";
+            blk4.style.backgroundColor = "#333";
+            div1.style.display = "none";
+            div2.style.display = "none";
+            div3.style.display = "block";
+            div4.style.display = "none";
+
+        }
+        if (pag == 4) {
+           
+            blk1.style.backgroundColor = "#333";
+            blk2.style.backgroundColor = "#333";
+            blk3.style.backgroundColor = "#333";
+            blk4.style.backgroundColor = "#4CAF50";
+            btc.style.display = "none";
+            div1.style.display = "none";
+            div2.style.display = "none";
+            div3.style.display = "none";
+            div4.style.display = "block";
+            btTer=rl.value;
+        }
+    });
+
 
     btc.addEventListener('click', () => {
         pag++;
@@ -174,6 +237,7 @@ const botonContinuar = () => {
 
         }
         if (pag == 4) {
+           
             blk1.style.backgroundColor = "#333";
             blk2.style.backgroundColor = "#333";
             blk3.style.backgroundColor = "#333";
@@ -183,11 +247,13 @@ const botonContinuar = () => {
             div2.style.display = "none";
             div3.style.display = "none";
             div4.style.display = "block";
+            btTer=rl.value;
         }
     });
 
 
 }
+
 const botonAtras = () => {
     bta.addEventListener('click', () => {
         pag--;
@@ -231,4 +297,3 @@ const botonAtras = () => {
 
 
 }
-

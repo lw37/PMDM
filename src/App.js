fb058dd4db616be6bacde6f7e7ccb48faf5a513c
Wei Luo <luo.luo.ll14@gmail.com>
2020-12-21 22:12:09 +0100

    Muestra lista de users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import {Card} from './Card';
+
 import { UserFind } from "./UserFind";
+import users from "./users.json";
 import userEvent from '@testing-library/user-event';
 /*<Card
 descripcion="Cyberpunk 2077 es un videojuego desarrollado y publicado por CD Projekt, que se lanzó para Microsoft Windows, PlayStation 4 y Xbox One el 10 de diciembre de 2020, y posteriormente en PlayStation 5, Xbox Series X|S y Google Stadia. "
@@ -10,17 +12,41 @@ apellido='Luo'
 nombre='Wei' 
 link='https://i.blogs.es/709902/cyberpunkvideo02/450_1000.jpeg'></Card>
 */
-const App=()=> {
-  return (
-    <div  className="App">
-      <header className="App-header">
-      <UserFind></UserFind>
-
-
-
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apellido: '',
+      nombre: '',
+      fechaNaci: Date.now(),
+      show: false
+    }
+  }
+  listaUser=()=> {
+  return  users.map((users, index) => {
+      return  <UserFind key={index} name={users.name} surname={users.surname} dateOfBirth={users.dateOfBirth}></UserFind>
+    })
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    if(event.target.value!=''){
+      this.setState({show:true})
+    }else{
+      this.setState({show:false})
+    }
+  }
+  render() {
+    return (
+      <>
+        <div className="App">
+          <header className="App-header">
+            <p>Apellido</p>
+            <input type='text' value={this.state.apellido} onChange={this.handleChange}></input>
+            {this.state.show && <button>Filtrar</button>}
+            {this.listaUser()}¡
+           </header>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
--- src/UserFind.js
+++ src/UserFind.js
@@ -1,36 +1,17 @@
-import userEvent from '@testing-library/user-event';
+
 import React from 'react';
 import './Card.css';
-import users from "./users.json";
-export class UserFind extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            apellido: '',
-            nombre:'',
-            fechaNaci: Date.now(),
-            show: false
-        }
-    }
 
-    listaUser=(users)=>{
-           users.map(user,index=>{ return(
-               <div>
-               <p  key={index}>Nombre:{user.name}</p>
-               <p  key={index}>Apellido:{user.surname}</p>
-               <p  key={index}>fechaNaci:{user.dateOfBirth}</p>
-            </div>)
-           })
-    }
+export class UserFind extends React.Component {
 
     render() {
         return (
             <div className={'Contenetor'}>
                 <div>
-                    {this.listaUser(users)}
-                   <input></input>
+                    <p>Nombre:{this.props.name}</p>
+                    <p>Apellido:{this.props.surname}</p>
+                    <p>fechaNaci:{this.props.dateOfBirth}</p>
                 </div>
-                
             </div>
         );
     };
